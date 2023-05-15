7ac9f497f34412f5221ee4da4dcd0ce63c28f67a
FunnPunn <84284151+FunnPunn@users.noreply.github.com>
2023-05-15 20:52:16 +0000

    upload to codespaces

---
+++ Website/circuits/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/workspaces/RecRoom-Circuits/Website/circuits/node_modules/infima/dist/css/default/default.css'),
+  require('/workspaces/RecRoom-Circuits/Website/circuits/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/workspaces/RecRoom-Circuits/Website/circuits/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/workspaces/RecRoom-Circuits/Website/circuits/src/css/custom.css'),
+];
