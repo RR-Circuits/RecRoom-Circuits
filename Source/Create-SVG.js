ecc47d6ee1feb09230ce24121c119e1330d09a6e
FunnPunn <84284151+FunnPunn@users.noreply.github.com>
2023-05-24 19:02:51 +0000

    grapher got a lot better

--- Source/Create-SVG.js
+++ Source/Create-SVG.js
@@ -20,37 +20,86 @@ const TopHeight = 41
 const PaddingFromTop = 8
 const PaddingFromBottom = 20
 
-const chipxoffset = 20
+const chipxoffset = 160
 
 //Padding
 
 function AppendPort(ParentObject, IsInput, PortType, [posx, posy]) {
-    const { HasDefaultValue, Model, Color } = Ports[PortType]
-    switch (Model) {
-        case "Exec":
-            ParentObject.append("path")
-                .attr("d", "M._posx,._posyh-16.465c-0.552,0,-1,0.448,-1,1v16c0,0.552,0.448,1,1,1h16.465c0.334,0,0.647,-0.167,0.832,-0.445l5.333,-8c0.224,-0.336,0.224,-0.774,0,-1.11l-5.333,-8c-0.185,-0.278,-0.498,-0.445,-0.832,-0.445z")
+    const PType = Ports[PortType]
+    var Color = "#F6EEE8"
+    var Model = "Data"
+    var HasDefaultValue = false
+    var portoffset = 5
+    try {
+        Color = PType["Color"]
+        Model = PType["Model"]
+        HasDefaultValue = Ports[PortType]["HasDefaultValue"]
+    }
+    catch (err) {
+        
+    }
+    if (Model == "Exec") {
+        ParentObject.append("path")
+            .attr("d", "M._posx,._posyh-16.465c-0.552,0,-1,0.448,-1,1v16c0,0.552,0.448,1,1,1h16.465c0.334,0,0.647,-0.167,0.832,-0.445l5.333,-8c0.224,-0.336,0.224,-0.774,0,-1.11l-5.333,-8c-0.185,-0.278,-0.498,-0.445,-0.832,-0.445z".replace("._posx", posx + portoffset).replace("._posy", posy))
+            .attr("fill", Color)
+        return 18
+    } else if (Model == "Data") {
+        if (HasDefaultValue && IsInput) {
+            ParentObject.append("g")
+                .attr("filter", "url(#filter0_di_2556_19132)")
+                .append("rect")
+                    .attr("x", posx - 12)
+                    .attr("y", posy)
+                    .attr("width", 22)
+                    .attr("height", "15")
+                    .attr("rx", "1")
+                    .attr("fill", Color)
+
+            const g2 = ParentObject.append("g")
+                .attr("filter", "url(#filter1_di_2556_19132)")
+            const TempWidth = 30
+            g2.append("rect")
+                .attr("x", posx - 24 - TempWidth - 12)
+                .attr("y", posy - 4)
+                .attr("width", 15 + TempWidth)
+                .attr("height", 23)
+                .attr("rx", "1")
                 .attr("fill", Color)
-        case "Data":
-            if (HasDefaultValue || !IsInput) {
-                ParentObject.append("g")
-                    .attr("filter", "url(#filter0_di_2556_19132)")
-                    .append("rect")
-                        .attr("x", posx)
-                        .attr("y", posy)
-                        .attr("width", 22)
-                        .attr("height", "")
-            } else {
 
-            }
+            g2.append("rect")
+                .attr("x", posx - 20 - TempWidth - 12)
+                .attr("y", posy + 0.001)
+                .attr("width", 7 + TempWidth)
+                .attr("height", 15)
+                .attr("rx", "1")
+                .attr("fill", "#818081")
 
+            ParentObject.append("g")
+                .attr("filter", "url(#filter2_di_2556_19132)")
+                .append("rect")
+                    .attr("x", posx - 9 - 12)
+                    .attr("y", posy + 4)
+                    .attr("width", 9)
+                    .attr("height", 7)
+                    .attr("fill", Color)
+
+        } else {
+            ParentObject.append("rect")
+                .attr("x", posx - 12)
+                .attr("y", posy)
+                .attr("width", "22")
+                .attr("height", "15")
+                .attr("rx", "1")
+                .attr("fill", Color)
+        }
+    return 15
     }
 }
 
 const Template = new jsdom.JSDOM('<body></body>')
 const NewChip = d3.select(Template.window.document.body).append("svg")
-    .attr("width", 165)
-    .attr("height", 165)
+    .attr("width", 400)
+    .attr("height", 400)
 
 const Top = NewChip
     .append("rect")
@@ -82,16 +131,12 @@ var InSpacing = TopHeight + PaddingFromTop
 var OutSpacing = TopHeight + PaddingFromTop
 
 for (const port of Funcs["Inputs"]) {
-    NewChip.append("path")
-        .attr("d", Exec.replace("._posx", chipxoffset + 5).replace("._posy", InSpacing))
-        .attr("fill", "#F55C1A")
-    InSpacing = InSpacing + PortHeight + VerticalPortPadding
+    const returnedwidth = AppendPort(NewChip, true, port["DataType"], [chipxoffset, InSpacing])
+    InSpacing = InSpacing + returnedwidth + VerticalPortPadding
 }
 for (const port of Funcs["Outputs"]) {
-    NewChip.append("path")
-        .attr("d", Exec.replace("._posx", chipxoffset + 111 + 5).replace("._posy", OutSpacing))
-        .attr("fill", "#F55C1A")
-    OutSpacing = OutSpacing + PortHeight + VerticalPortPadding
+    const returnedwidth = AppendPort(NewChip, false, port["DataType"], [chipxoffset + 111, OutSpacing])
+    OutSpacing = OutSpacing + returnedwidth + VerticalPortPadding
 }
 
 Bottom.attr("height", PaddingFromTop + PaddingFromBottom + Math.max(TotalInputSpacing, TotalOutputSpacing))
