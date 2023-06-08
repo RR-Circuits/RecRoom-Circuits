8a53c3e36c5a90756fc33eb8f85f2aed95eb5bf5
joksulainen <54633559+joksulainen@users.noreply.github.com>
2023-06-08 19:31:36 +0300

    Update docusaurus.config.js

--- Circuits/docusaurus.config.js
+++ Circuits/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://FunnPunn.github.io',
+  url: 'https://circuits.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'FunnPunn', // Usually your GitHub org/user name.
-  projectName: 'FunnPunn.github.io', // Usually your repo name.
+  projectName: 'RecRoom-Circuits', // Usually your repo name.
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
